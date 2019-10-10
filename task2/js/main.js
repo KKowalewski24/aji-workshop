14510dbd87b2b6a86483918b7fc001f69f87d256
Kamil Kowalewski <szamil128@onet.pl>
2019-10-10 20:55:39 +0200

    Add GET and POST + code refactor

--- task2/js/main.js
+++ task2/js/main.js
@@ -1,54 +1,90 @@
 "use strict";
 
+/*------------------------ VARIABLE ------------------------*/
 const ITEMS = "items";
+const FIREBASE_LINK = "https://schedulelist-d5850.firebaseio.com/.json";
+const FIREBASE_KEY = "ihKtSAJNoNNsSLI4rdiVCcub5PuMeY5eIqnLGXpR";
 let itemList = [];
 
-let initData = () => {
-  let jsonList = window.localStorage.getItem(ITEMS);
-
-  if (jsonList != null) {
-    itemList = JSON.parse(jsonList);
-  } else {
-    itemList.push(
-      {
-        title: "Learn JS",
-        description: "Create a demo application for my TODO's",
-        place: "445",
-        dueDate: new Date(2019, 10, 16)
-      },
-      {
-        title: "Lecture test",
-        description: "Quick test from the first three lectures",
-        place: "F6",
-        dueDate: new Date(2019, 10, 17)
+/*------------------------ GET | POST ------------------------*/
+let getJsonData = () => {
+  $.ajax({
+    url: FIREBASE_LINK,
+    type: 'GET',
+    headers: {
+      'secret-key': FIREBASE_KEY
+    },
+    success: (data) => {
+      if (data != null) {
+        itemList = data;
+        console.log(data);
       }
-    );
-  }
+    },
+    error: (err) => {
+      console.log(err.responseJSON);
+    }
+  })
+};
+
+let postJsonData = () => {
+  $.ajax({
+    url: FIREBASE_LINK,
+    type: 'PUT',
+    headers: {
+      'secret-key': FIREBASE_KEY
+    },
+    contentType: 'application/json',
+    data: JSON.stringify(itemList),
+    success: (data) => {
+      console.log(data);
+    },
+    error: (err) => {
+      console.log(err.responseJSON);
+    }
+  })
+};
+
+/*------------------------ ADD ITEM ------------------------*/
+let addItem = () => {
+  let item = {
+    title: document.getElementById("inputTitle").value,
+    description: document.getElementById("inputDescription").value,
+    place: document.getElementById("inputPlace").value,
+    dueDate: document.getElementById("inputDate").value
+  };
+
+  itemList.push(item);
+  window.localStorage.setItem(ITEMS, JSON.stringify(itemList));
+  postJsonData();
+};
+
+/*------------------------ UPDATE LIST ------------------------*/
+let deleteItem = (index) => {
+  itemList.splice(index, 1);
+  postJsonData();
+};
+
+let prepareDeleteButton = (it) => {
+  let deleteButton = document.createElement("input");
+  deleteButton.type = "button";
+  deleteButton.value = "x";
+  deleteButton.addEventListener("click", () => {
+    deleteItem(it);
+  });
+
+  return deleteButton;
 };
 
 let updateList = () => {
   let displayList = document.getElementById("displayList");
   let inputSearch = document.getElementById("inputSearch");
 
-  let deleteItem = (index) => {
-    itemList.splice(index, 1);
-  };
-
-  let prepareDeleteButton = (it) => {
-    let deleteButton = document.createElement("input");
-    deleteButton.type = "button";
-    deleteButton.value = "x";
-    deleteButton.addEventListener("click", () => {
-      deleteItem(it);
-    });
-
-    return deleteButton;
-  };
-
+  /*----- REMOVE CHILDS -----*/
   while (displayList.firstChild) {
     displayList.removeChild(displayList.firstChild);
   }
 
+  /*----- MAIN LOOP -----*/
   for (let it in itemList) {
     let div = document.createElement("div");
     let pElement = document.createElement("p");
@@ -78,17 +114,31 @@ let updateList = () => {
   }
 };
 
-let addItem = () => {
-  let item = {
-    title: document.getElementById("inputTitle").value,
-    description: document.getElementById("inputDescription").value,
-    place: document.getElementById("inputPlace").value,
-    dueDate: document.getElementById("inputDate").value
-  };
-
-  itemList.push(item);
-  window.localStorage.setItem(ITEMS, JSON.stringify(itemList));
-};
-
-initData();
+/*------------------------ FUNCTION CALL ------------------------*/
+// initData();
 setInterval(updateList, 1000);
+getJsonData();
+
+/*----- LOCAL STORAGE DO NOT DELETE -----*/
+// let initData = () => {
+//   let jsonList = window.localStorage.getItem(ITEMS);
+//
+//   if (jsonList != null) {
+//     itemList = JSON.parse(jsonList);
+//   } else {
+//     itemList.push(
+//       {
+//         title: "Learn JS",
+//         description: "Create a demo application for my TODO's",
+//         place: "445",
+//         dueDate: new Date(2019, 10, 16)
+//       },
+//       {
+//         title: "Lecture test",
+//         description: "Quick test from the first three lectures",
+//         place: "F6",
+//         dueDate: new Date(2019, 10, 17)
+//       }
+//     );
+//   }
+// };
