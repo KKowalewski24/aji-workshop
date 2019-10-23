aa752c4e5e0f997840a4c66e53b8c27f0082f7cf
Kamil Kowalewski <szamil128@onet.pl>
2019-10-23 17:55:08 +0200

    Fix mistakes in weather app

--- vue-exercise/vue-weather-forecast/src/App.vue
+++ vue-exercise/vue-weather-forecast/src/App.vue
@@ -1,45 +1,45 @@
 <template>
   <div id="app">
-    <Header msg="Awesome Weather App"/>
+    <Header msg="Awesome Weather App"></Header>
     <!--    <SearchCity/>-->
-    <div class="row">
-      <div>
-        <input type="text" placeholder="pick city" v-model="city">
+    <div class="container">
+      <div class="card row mt-5">
+        <div class="card-body">
+          <input type="text" placeholder="pick city" v-model="city">
+          <input type="button" value="check weather" v-on:click="check()">
+        </div>
       </div>
-      <div>
-        <input type="button" value="check weather" v-on:click="check()">
-      </div>
-    </div>
 
-    <div v-if="info">
-      <div class="row">
-        <h1>
-          {{info.data[0].city_name}}
-        </h1>
-        <h4>
-          {{info.data[0].weather.description}}
-        </h4>
+      <div v-if="info">
+        <div class="card row mt-5">
+          <div class="card-body">
+            <h1>{{info.data[0].city_name}}</h1>
+            <h4>{{info.data[0].weather.description}}</h4>
+          </div>
+        </div>
       </div>
+
+      <h1>Current weather JSON</h1>
+
+      <pre>{{info}}</pre>
+
+      <h1>
+        Forecast JSON
+      </h1>
+
+      <pre>{{forecast}}</pre>
+
     </div>
-    <h1>
-      Current weather JSON
-    </h1>
-    <pre>
-  {{info}}
-  </pre>
-    <h1>
-      Forecast JSON
-    </h1>
-    <pre>
-    {{forecast}}
-  </pre>
   </div>
 </template>
 
 <script>
   import Header from "./components/Header";
   import {AxiosInstance as axios} from "axios";
-  // import SearchCity from "./components/SearchCity";
+
+  const WEATHER_BIT = "https://api.weatherbit.io/v2.0/current?city=";
+  const WEATHER_BIT_3HRS = "https://api.weatherbit.io/v2.0/forecast/3hourly?city=";
+  const WEATHER_KEY = "&key=YOUR_API_KEY_HERE";
 
   export default {
     name: 'app',
@@ -64,11 +64,10 @@
       },
       check: function () {
         let self = this;
-        axios.get('https://api.weatherbit.io/v2.0/current?city=' + self.city + '&key=YOUR_API_KEY_HERE')
+        axios.get(WEATHER_BIT + self.city + WEATHER_KEY)
           .then(response => (self.info = response.data));
-        axios.get('https://api.weatherbit.io/v2.0/forecast/3hourly?city=' + self.city + '&key=YOUR_API_KEY_HERE').then(response => {
-          self.forecast = response.data
-        });
+        axios.get(WEATHER_BIT_3HRS + self.city + WEATHER_KEY)
+          .then(response => self.forecast = response.data);
       }
     }
   }
@@ -79,6 +78,5 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-
   }
 </style>
