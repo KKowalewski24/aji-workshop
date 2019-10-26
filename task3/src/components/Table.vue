9406330fb9aa340aa2ea9e318b9595e8abdb6adf
Kamil Kowalewski <szamil128@onet.pl>
2019-10-26 14:55:26 +0200

    Change mdb to DOM element

--- task3/src/components/Table.vue
+++ task3/src/components/Table.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <SearchMovie/>
+    <SearchMovie :searchProperties="setSearchProperties"/>
     <MovieTable :jsonData="fullJsonData"/>
   </div>
 </template>
 
 <script>
-
   import SearchMovie from "./table/SearchMovie";
   import MovieTable from "./table/MovieTable";
 
@@ -18,6 +17,24 @@
     },
     props: {
       fullJsonData: Array
+    },
+    data() {
+      return {
+        searchProperties: {
+          title: "",
+          dateFrom: "",
+          dateTo: "",
+          cast: ""
+        }
+      }
+    },
+    methods: {
+      setSearchProperties: function (title, dateFrom, dateTo, cast) {
+        this.searchProperties.title = title;
+        this.searchProperties.dateFrom = dateFrom;
+        this.searchProperties.dateTo = dateTo;
+        this.searchProperties.cast = cast;
+      }
     }
   }
 </script>
--- task3/src/components/table/SearchMovie.vue
+++ task3/src/components/table/SearchMovie.vue
@@ -1,42 +1,36 @@
 <template>
-  <mdb-card class="mt-5">
-    <mdb-card-body>
-      <mdb-list-group>
-        <mdb-list-group-item>
+  <div class="card mt-5">
+    <div class="card-body">
+      <li class="list-group">
+        <ul class="list-group-item">
           <input type="text" id=inputTitle class="form-control" placeholder="Title"/>
-        </mdb-list-group-item>
+        </ul>
 
-        <mdb-list-group-item>
+        <ul class="list-group-item">
           <input type="text" id=inputProductionFrom class="form-control"
                  placeholder="Production year from:"/>
-        </mdb-list-group-item>
+        </ul>
 
-        <mdb-list-group-item>
+        <ul class="list-group-item">
           <input type="text" id=inputProductionTo class="form-control"
                  placeholder="Production year to:"/>
-        </mdb-list-group-item>
+        </ul>
 
-        <mdb-list-group-item>
+        <ul class="list-group-item">
           <input type="text" id="inputCast" class="form-control" placeholder="Cast"/>
-        </mdb-list-group-item>
+        </ul>
 
-        <mdb-btn class="m-0" color="mdb-color">Search</mdb-btn>
-      </mdb-list-group>
-    </mdb-card-body>
-  </mdb-card>
+        <button class="btn mdb-color white-text m-0">Search</button>
+      </li>
+    </div>
+  </div>
 </template>
 
 <script>
-  import {mdbCard, mdbListGroup, mdbListGroupItem, mdbBtn, mdbCardBody} from 'mdbvue';
-
   export default {
     name: "SearchMovie",
-    components: {
-      mdbCard,
-      mdbListGroup,
-      mdbCardBody,
-      mdbBtn,
-      mdbListGroupItem
+    props: {
+      searchProperties: Function
     }
   }
 </script>
