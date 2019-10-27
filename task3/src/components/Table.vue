289c128629cf245f1e9b8ada1245d220b95717b5
Kamil Kowalewski <szamil128@onet.pl>
2019-10-27 18:43:40 +0100

    Add logic to search in json array

--- task3/src/components/Table.vue
+++ task3/src/components/Table.vue
@@ -4,7 +4,10 @@
         :properties="searchProperties"
         :searchItems="searchMovies"
     />
-    <MovieTable :jsonData="filteredJsonData" :key="reRenderKey"/>
+    <MovieTable
+        :jsonData="filteredJsonData"
+        :key="reRenderedKey"
+    />
   </div>
 </template>
 
@@ -15,46 +18,73 @@
 
   export default {
     name: "Table",
+
     components: {
       MovieTable,
       SearchMovie
     },
+
     props: {
       fullJsonData: Array
     },
+
     data() {
       return {
+        reRenderedKey: 0,
+        filteredJsonData: _.cloneDeep(this.fullJsonData),
         searchProperties: {
           title: "",
           dateFrom: "",
           dateTo: "",
           cast: ""
         },
-        filteredJsonData: _.cloneDeep(this.fullJsonData),
-        reRenderKey: 0
       }
     },
+
     methods: {
-      searchMovies: function () {
+      isFilledInputs: function () {
+        //IF INPUTS ARE EMPTY RETURN FALSE IF NOT RETURN TRUE
         if (!(this.searchProperties.title === ""
           && this.searchProperties.dateFrom === ""
           && this.searchProperties.dateTo === ""
           && this.searchProperties.cast === "")) {
-
-          this.filteredJsonData = [
-            {
-              "title": "After Dark in Central Park",
-              "year": 1900,
-              "cast": [],
-              "genres": []
-            }
-          ]
-
-          this.reRenderKey += 1;
+          return true
         }
 
-      }
-    }
+        return false;
+      },
+
+      getLowerCaseValue: function (msg) {
+        return _.toLower(msg);
+      },
+
+      isIncluded: function (arrayValue, inputValue) {
+        return _.includes(this.getLowerCaseValue(arrayValue), this.getLowerCaseValue(inputValue))
+      },
+
+      checkInputs: function (item) {
+        if (this.isIncluded(item.title, this.searchProperties.title)
+          && this.isIncluded(item.dateFrom, this.searchProperties.dateFrom)
+          && this.isIncluded(item.dateTo, this.searchProperties.dateTo)
+          && this.isIncluded(item.cast, this.searchProperties.cast)) {
+          return true;
+        }
+
+        return false
+      },
+
+      searchMovies: function () {
+        for (let it in this.fullJsonData) {
+          if (this.isFilledInputs() && this.checkInputs(this.fullJsonData[it])) {
+            alert(this.fullJsonData[it].title);
+
+            this.reRenderedKey += 1;
+          }
+        }
+      },
+
+    },
+
   }
 </script>
 
