2906986a47eed66d58a680b304ca7a994e3a25ce
Kamil Kowalewski <szamil128@onet.pl>
2019-11-02 19:17:26 +0100

    Add displaying data from backend

--- node-exercise/client/src/components/Products.vue
+++ node-exercise/client/src/components/Products.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(it,index) in products" :key="index">
+        <td>{{it.name}}</td>
+        <td>{{it.description}}</td>
+        <td>{{it.price}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+  import axios from "axios";
+
   export default {
-    name: "Products"
+    name: "Products",
+
+    data() {
+      return {
+        products: [],
+      }
+    },
+
+    mounted() {
+      let self = this;
+      axios.get("http://127.0.0.1:8081/products")
+        .then((res) => self.products = res.data)
+    },
+
   }
 </script>
 
--- node-exercise/server/src/app.js
+++ node-exercise/server/src/app.js
@@ -5,6 +5,12 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
