4f10f04b2fc981758cff8d7190a408b1765d41bd
Kamil Kowalewski <szamil128@onet.pl>
2019-11-02 18:26:31 +0100

    Add data + controllers

--- node-exercise/src/routes/index.js
+++ node-exercise/src/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const IndexController = require('../controller/IndexController');
+const ProductsController = require("../controller/ProductsController");
 
-router.get('/', (req, res) => {
-  res.json({'status': 'working!'});
-});
+router.get('/', IndexController.home);
+router.get("/products", ProductsController.getAll);
+router.get("/products/:id", ProductsController.getById);
+router.post("/products", ProductsController.store);
+router.put("/products/:id", ProductsController.updateById);
 
 module.exports = router;
